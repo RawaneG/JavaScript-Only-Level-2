8a1761634fb46e35c0d46561768a337b15ee5040
Rawane <rawane.meissa@gmail.com>
2022-03-20 01:54:50 +0000

    TP3

--- TP 2/script.js
+++ TP 2/script.js
@@ -1,93 +1,111 @@
 const bouton = document.getElementById('bouton');
 const menu = document.querySelector('.menu');
 let liste = document.createElement('div');
-liste.setAttribute('class','liste');
+liste.setAttribute('class', 'liste');
 
-let tableau = 
-[
-    {
-        titre : "Dashboard",
-        className : 'tableauDeBord',
-        liste : ['Dashboard v1','Dashboard v2','Dashboard v3'],
-        iconePrincipal : 'fa fa-tachometer-alt',
-        angleLeft : 'fa fa-angle-left',
-        angleDown : 'fa fa-angle-down'
-    },
-    {
-        titre : "Widgets",
-        className : 'widgets',
-        liste : [],
-        iconePrincipal : 'fa fa-th aria-hidden="true"',
-        angleLeft : 'fa fa-angle-left',
-        angleDown : 'fa fa-angle-down'
-    },
-    {
-        titre : "Layout Options",
-        className : 'layoutOptions',
-        liste : ['ChartJS','Flot','Inline','uPlot'],
-        iconePrincipal : 'fas fa-copy',
-        angleLeft : 'fa fa-angle-left',
-        angleDown : 'fa fa-angle-down'
-    },
-    {
-        titre : "Charts",
-        className : 'charts',
-        liste : [],
-        iconePrincipal : 'fas fa-chart-pie',
-        angleLeft : 'fa fa-angle-left',
-        angleDown : 'fa fa-angle-down'
-    },
-    {
-        titre : "UI Elements",
-        className : 'ui_Elements',
-        liste : [],
-        iconePrincipal : 'fas fa-tree',
-        angleLeft : 'fa fa-angle-left',
-        angleDown : 'fa fa-angle-down'
-    },
-    {
-        titre : "Forms",
-        className : 'forms',
-        liste : [],
-        iconePrincipal : 'fas fa-edit',
-        angleLeft : 'fa fa-angle-left',
-        angleDown : 'fa fa-angle-down'
-    },
-    {
-        titre : "Tables",
-        className : 'tables',
-        liste : [],
-        iconePrincipal : 'fas fa-table',
-        angleLeft : 'fa fa-angle-left',
-        angleDown : 'fa fa-angle-down'
-    }
-];
-
-bouton.addEventListener('click',() => 
+bouton.addEventListener('click', () => 
 {
     menu.classList.toggle('menu');
     menu.classList.toggle('active');
 });
 
-for(let i = 0; i < tableau.length; i++)
+let tableau =
+    [
+        {
+            titre: "Dashboard",
+            className: 'tableauDeBord',
+            liste: ['Dashboard v1', 'Dashboard v2', 'Dashboard v3'],
+            iconePrincipal: 'fa fa-tachometer-alt',
+            angleLeft: 'fa fa-angle-left',
+            angleDown: 'fa fa-angle-down'
+        },
+        {
+            titre: "Widgets",
+            className: 'widgets',
+            liste: [],
+            iconePrincipal: 'fa fa-th aria-hidden="true"',
+            angleLeft: 'fa fa-angle-left',
+            angleDown: 'fa fa-angle-down'
+        },
+        {
+            titre: "Layout Options",
+            className: 'layoutOptions',
+            liste: ['ChartJS', 'Flot', 'Inline', 'uPlot'],
+            iconePrincipal: 'fas fa-copy',
+            angleLeft: 'fa fa-angle-left',
+            angleDown: 'fa fa-angle-down'
+        },
+        {
+            titre: "Charts",
+            className: 'charts',
+            liste: [],
+            iconePrincipal: 'fas fa-chart-pie',
+            angleLeft: 'fa fa-angle-left',
+            angleDown: 'fa fa-angle-down'
+        },
+        {
+            titre: "UI Elements",
+            className: 'ui_Elements',
+            liste: [],
+            iconePrincipal: 'fas fa-tree',
+            angleLeft: 'fa fa-angle-left',
+            angleDown: 'fa fa-angle-down'
+        },
+        {
+            titre: "Forms",
+            className: 'forms',
+            liste: [],
+            iconePrincipal: 'fas fa-edit',
+            angleLeft: 'fa fa-angle-left',
+            angleDown: 'fa fa-angle-down'
+        },
+        {
+            titre: "Tables",
+            className: 'tables',
+            liste: [],
+            iconePrincipal: 'fas fa-table',
+            angleLeft: 'fa fa-angle-left',
+            angleDown: 'fa fa-angle-down'
+        }
+    ];
+
+for (let i = 0; i < tableau.length; i++) 
 {
     let divPrincipal = document.createElement('div');
-    divPrincipal.classList.add(tableau[i].className,'principal');
+    divPrincipal.classList.add(tableau[i].className, 'principal');
     let icone_principal = document.createElement('i');
-    icone_principal.setAttribute('class',tableau[i].iconePrincipal);
+    icone_principal.setAttribute('class', tableau[i].iconePrincipal);
     let ensemble_icon = document.createElement('div');
-    ensemble_icon.setAttribute('class','ensemble_icon');
+    ensemble_icon.setAttribute('class', 'ensemble_icon');
     let currentElement = document.createElement('span');
     currentElement.innerHTML = tableau[i].titre;
     let count = document.createElement('div');
-    count.setAttribute('class','count');
+    count.setAttribute('class', 'count');
     let countSpan = document.createElement('span');
     let newSpan = document.createElement('span');
     let angleLeft = document.createElement('i');
-    angleLeft.setAttribute('class',tableau[i].angleLeft);
+    angleLeft.setAttribute('class', tableau[i].angleLeft);
+    angleLeft.className += ' arrowLeft';
     let angleDown = document.createElement('i');
-    angleDown.setAttribute('class',tableau[i].angleDown);
-    angleDown.setAttribute('class','angle_visible');
+    let miniMenu = document.createElement('div');
+    miniMenu.classList.add('mini_menu');
+    let ul = document.createElement('ul');
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    let circleSpan = document.createElement('span');
+    circleSpan.classList.add('circle');
+    let paraSpan = document.createElement('span');
+    let circleTable = [];
+    for(let j = 0; j < tableau[i].liste.length; j++)
+    {   
+
+    }
+
+    li.appendChild(a);
+    ul.appendChild(li);
+    miniMenu.appendChild(ul);
+    angleDown.setAttribute('class', tableau[i].angleDown);
+    angleDown.setAttribute('class', 'angle_invisible');
     count.appendChild(countSpan);
     count.appendChild(newSpan);
     count.appendChild(angleDown);
@@ -96,15 +114,34 @@ for(let i = 0; i < tableau.length; i++)
     ensemble_icon.appendChild(count);
     divPrincipal.appendChild(icone_principal);
     divPrincipal.appendChild(ensemble_icon);
-    if(divPrincipal.className == 'tableauDeBord principal')
+
+    if (divPrincipal.className == 'tableauDeBord principal') 
     {
         divPrincipal.classList.add('bg_color');
     }
-    if(divPrincipal.className == 'widgets principal')
+    if (divPrincipal.className == 'widgets principal') 
     {
         newSpan.innerHTML = 'New';
         newSpan.classList.add('new');
+        angleLeft.remove();
+    }
+    if (divPrincipal.className == 'layoutOptions principal') 
+    {
+        countSpan.innerHTML = 6;
+        countSpan.classList.add('counter');
     }
     liste.appendChild(divPrincipal);
+    liste.appendChild(miniMenu);
     menu.appendChild(liste);
-}   
+}
+
+let principal = document.querySelectorAll('.principal');
+
+principal[0].addEventListener('mouseover',() => 
+{
+    if(menu.className == 'active')
+    {
+        menu.classList.remove('active');
+        menu.classList.add('menu');
+    }
+});
---
+++ TP 3/script.js
@@ -0,0 +1,40 @@
+const btnGauche = document.getElementById('btn_gauche');
+const btnDroite = document.getElementById('btn_droite');
+const images = document.querySelectorAll('img');
+let imageActuel = 0;
+
+btnGauche.addEventListener('click', imagePrecedente);
+btnDroite.addEventListener('click', imageSuivante);
+
+function imageSuivante()
+{
+    images[imageActuel].classList.remove('active');
+
+    if(imageActuel < images.length - 1)
+    {
+        imageActuel++;
+    }
+    else
+    {
+        imageActuel = 0
+    }
+
+    images[imageActuel].classList.add('active');
+}
+
+function imagePrecedente()
+{
+    images[imageActuel].classList.remove('active');
+
+    if(imageActuel > 0)
+    {
+        imageActuel--;
+    }
+    else
+    {
+        imageActuel = images.length-1;
+    }
+    
+    images[imageActuel].classList.add('active');
+}
+
