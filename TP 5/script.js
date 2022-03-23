2449a440b181310db008a24722c7ddf60905632a
Rawane <rawane.meissa@gmail.com>
2022-03-23 18:14:29 +0000

    update

--- TP 5/script.js
+++ TP 5/script.js
@@ -1,9 +1,34 @@
-const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-
+let i = 1;
 const papa = document.querySelector('.movies');
 const input = document.querySelector('input');
+
+window.addEventListener('scroll', () => 
+{
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	
+	console.log( { scrollTop, scrollHeight, clientHeight });
+	
+	if(clientHeight + scrollTop >= scrollHeight - 5) 
+    {
+		// show the loading animation
+		fetch(APIURL + (i +=1))
+        .then((element) => 
+        {
+            return element.json();
+        })
+        .then(element => 
+                {
+                    element.results.forEach(element => 
+                        {
+                            let image = IMGPATH + element.poster_path;
+                            creerDiv(image ,element.original_title,element.vote_average,element.overview);
+                        })
+                });
+	}
+});
 
 function fetchingAPI(element)
 {
