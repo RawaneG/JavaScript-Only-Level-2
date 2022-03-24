9ec55f79528fea191d68d5d6b7d4476976cb7095
Rawane <rawane.meissa@gmail.com>
2022-03-24 10:14:31 +0000

    update

--- TP 2/script.js
+++ TP 2/script.js
@@ -66,64 +66,69 @@ let tableau =
         }
     ];
 
-for (let i = 0; i < tableau.length; i++) 
+function creation(tableau)
 {
-    let divPrincipal = document.createElement('div');
-    divPrincipal.classList.add(tableau[i].className, 'principal');
-    let icone_principal = document.createElement('i');
-    icone_principal.setAttribute('class', tableau[i].iconePrincipal);
-    let ensemble_icon = document.createElement('div');
-    ensemble_icon.setAttribute('class', 'ensemble_icon');
-    let currentElement = document.createElement('span');
-    currentElement.innerHTML = tableau[i].titre;
-    let count = document.createElement('div');
-    count.setAttribute('class', 'count');
-    let countSpan = document.createElement('span');
-    let newSpan = document.createElement('span');
-    let angleLeft = document.createElement('i');
-    angleLeft.setAttribute('class', tableau[i].angleLeft);
-    angleLeft.className += ' arrowLeft';
-    let miniMenu = document.createElement('div');
-    miniMenu.classList.add('mini_menu');
-    let ul = document.createElement('ul');
-    let li = document.createElement('li');
-    for(let j = 0; j < tableau[i].liste.length; j++)
-    {   
-        li.innerHTML += 
-        `<a href="">
-        <span class="circle"></span>
-        <span>${tableau[i].liste[j]}</span>
-        </a>`;
-    }
-    ul.appendChild(li);
-    miniMenu.appendChild(ul);
-    count.appendChild(countSpan);
-    count.appendChild(newSpan);
-    count.appendChild(angleLeft);
-    ensemble_icon.appendChild(currentElement);
-    ensemble_icon.appendChild(count);
-    divPrincipal.appendChild(icone_principal);
-    divPrincipal.appendChild(ensemble_icon);
-
-    if (divPrincipal.className == 'tableauDeBord principal') 
+    for (let i = 0; i < tableau.length; i++) 
     {
-        divPrincipal.classList.add('bg_color');
+        let divPrincipal = document.createElement('div');
+        divPrincipal.classList.add(tableau[i].className, 'principal');
+        let icone_principal = document.createElement('i');
+        icone_principal.setAttribute('class', tableau[i].iconePrincipal);
+        let ensemble_icon = document.createElement('div');
+        ensemble_icon.setAttribute('class', 'ensemble_icon');
+        let currentElement = document.createElement('span');
+        currentElement.innerHTML = tableau[i].titre;
+        let count = document.createElement('div');
+        count.setAttribute('class', 'count');
+        let countSpan = document.createElement('span');
+        let newSpan = document.createElement('span');
+        let angleLeft = document.createElement('i');
+        angleLeft.setAttribute('class', tableau[i].angleLeft);
+        angleLeft.className += ' arrowLeft';
+        let miniMenu = document.createElement('div');
+        miniMenu.classList.add('mini_menu');
+        let ul = document.createElement('ul');
+        let li = document.createElement('li');
+        for(let j = 0; j < tableau[i].liste.length; j++)
+        {   
+            li.innerHTML += 
+            `<a href="">
+            <span class="circle"></span>
+            <span>${tableau[i].liste[j]}</span>
+            </a>`;
+        }
+        ul.appendChild(li);
+        miniMenu.appendChild(ul);
+        count.appendChild(countSpan);
+        count.appendChild(newSpan);
+        count.appendChild(angleLeft);
+        ensemble_icon.appendChild(currentElement);
+        ensemble_icon.appendChild(count);
+        divPrincipal.appendChild(icone_principal);
+        divPrincipal.appendChild(ensemble_icon);
+    
+        if (divPrincipal.className == 'tableauDeBord principal') 
+        {
+            divPrincipal.classList.add('bg_color');
+        }
+        if (divPrincipal.className == 'widgets principal') 
+        {
+            newSpan.innerHTML = 'New';
+            newSpan.classList.add('new');
+            angleLeft.remove();
+        }
+        if (divPrincipal.className == 'layoutOptions principal') 
+        {
+            countSpan.innerHTML = tableau[i].liste.length;
+            countSpan.classList.add('counter');
+        }
+        liste.appendChild(divPrincipal);
+        liste.appendChild(miniMenu);
+        menu.appendChild(liste);
     }
-    if (divPrincipal.className == 'widgets principal') 
-    {
-        newSpan.innerHTML = 'New';
-        newSpan.classList.add('new');
-        angleLeft.remove();
-    }
-    if (divPrincipal.className == 'layoutOptions principal') 
-    {
-        countSpan.innerHTML = tableau[i].liste.length;
-        countSpan.classList.add('counter');
-    }
-    liste.appendChild(divPrincipal);
-    liste.appendChild(miniMenu);
-    menu.appendChild(liste);
 }
+
+creation(tableau);
 
 let principal = document.querySelectorAll('.principal');
 let miniMenu = document.querySelectorAll('.mini_menu');
